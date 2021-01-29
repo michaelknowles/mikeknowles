ed67bed4e760c0cf9c0df460daeb79031ef0c053
Michael Knowles <mikewknowles@outlook.com>
2021-01-28 22:29:41 -0800

    init

---
+++ readme.md
@@ -0,0 +1,13 @@
+# mikeknowl.es
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/9ec81cc9-a9fe-457c-94a9-271f207bdf23/deploy-status)](https://app.netlify.com/sites/zen-poincare-644253/deploys)
+
+My personal website.
+
+## Dependencies
+
+- Hugo
+- Tailwindcss
+- Tailwindcss-forms
+- PostCSS
+- Autoprefixer
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+// tailwind.config.js
+
+module.exports = {
+  purge: {
+    enabled: process.env.HUGO_ENVIRONMENT === "production",
+    content: ["./*.html"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      display: ["Source Sans Pro", "Helvetica", "Arial", "sans-serif"],
+      body: ["Lato", "Helvetica", "Arial", "sans-serif"],
+    },
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 0.5s ease-in forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+    },
+  },
+  variants: {
+    animation: ["motion-safe"],
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
